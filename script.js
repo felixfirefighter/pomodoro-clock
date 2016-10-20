684646779d24cf0d6610ee4c73adbb368ce3d062
YoongTi <leeyoongti@gmail.com>
2016-10-19 23:51:43 -0400

    Added break clock

--- script.js
+++ script.js
@@ -2,6 +2,57 @@
  * Created by LEE on 2016/10/19.
  */
 
-var clock = $('.clock').FlipClock(1500,{
-    countdown: true
+$(document).ready(function () {
+    var startButton = $("#btn-start");
+    var resetButton = $("#btn-reset");
+
+    var isStop = true;
+
+    var time = 1500;
+    var becomeReset = false;
+
+    var sessionClock = $('.session-clock').FlipClock(time, {
+        clockFace: 'MinuteCounter',
+
+        countdown: true,
+        autoStart: false,
+
+        callbacks: {
+            interval: function () {
+                time = sessionClock.getTime().time;
+            }
+        }
+    });
+
+    var breakClock = $('.break-clock').FlipClock(time, {
+        clockFace: 'MinuteCounter',
+
+        countdown: true,
+        autoStart: false,
+
+        callbacks: {
+            interval: function () {
+                time = breakClock.getTime().time;
+            }
+        }
+    });
+
+    startButton.click(function () {
+        if (isStop) {
+            sessionClock.start();
+            startButton.text("Stop");
+
+            isStop = false;
+        } else {
+            sessionClock.stop();
+            startButton.text("Start");
+
+            isStop = true;
+        }
+    });
+
+    resetButton.click(function () {
+        //stop the countdown when reset is clicked
+    });
+
 });
