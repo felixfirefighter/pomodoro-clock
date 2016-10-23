d38f56ed9e301fafa6270f465a567ef67e406d53
YoongTi <leeyoongti@gmail.com>
2016-10-23 16:33:09 -0400

    Added button functions for all four time editing buttons

--- script.js
+++ script.js
@@ -9,14 +9,12 @@ $(document).ready(function () {
     var isSessionStop = true;
     var isBreakStop = true;
 
-    //user choice of break time, used to reset
-    var sessionTimeSet = 5;
-    var breakTimeSet = 5;
+    var resetSession = 1500;
+    var resetBreak = 300;
 
-    var sessionTime = 5;
-    var breakTime = 5;
-
-    var becomeReset = false;
+    //user choice of session and break time
+    var sessionTime = 1500;
+    var breakTime = 300;
 
     var sessionClock = $('.session-clock').FlipClock(sessionTime, {
         clockFace: 'MinuteCounter',
@@ -29,15 +27,15 @@ $(document).ready(function () {
                 sessionTime = sessionClock.getTime().time;
                 if (sessionTime == 0) {
                     //set time again to offset the one second difference
-                    breakClock.setTime(breakTimeSet+1);
+                    breakClock.setTime(breakTime + 1);
                     breakClock.start();
                     isBreakStop = false;
 
                     isSessionStop = true;
-                }else if(breakTime == 0 && isBreakStop){
+                } else if (breakTime == 0 && isBreakStop) {
 
                     //set time for display
-                    breakClock.setTime(breakTimeSet);
+                    breakClock.setTime(breakTime);
                 }
             }
         }
@@ -56,19 +54,68 @@ $(document).ready(function () {
                 if (breakTime == 0) {
 
                     //set time again to offset the one second difference
-                    sessionClock.setTime(sessionTimeSet+1);
+                    sessionClock.setTime(sessionTime + 1);
                     sessionClock.start();
-                    isSessionStop= false;
+                    isSessionStop = false;
 
                     isBreakStop = true;
-                }else if(sessionTime == 0 && isSessionStop){
+                } else if (sessionTime == 0 && isSessionStop) {
 
                     //set time for display
-                    sessionClock.setTime(sessionTimeSet);
+                    sessionClock.setTime(sessionTime);
                 }
             }
         }
     });
+
+    $("#btn-reduce-session-minute").click(function () {
+        changeSessionTime(-60);
+    });
+
+    $("#btn-increase-session-minute").click(function () {
+        changeSessionTime(60);
+    });
+
+    $("#btn-reduce-session-second").click(function () {
+        changeSessionTime(-10);
+    });
+
+    $("#btn-increase-session-second").click(function () {
+        changeSessionTime(10);
+    });
+
+    function changeSessionTime(time) {
+        if ((sessionTime + time >= 0) && (sessionTime + time <= 6000)) {
+            stopAllClocks();
+            sessionTime += time;
+            sessionClock.setTime(sessionTime);
+        }
+    }
+
+    $("#btn-reduce-break-minute").click(function () {
+        changeBreakTime(-60);
+    });
+
+    $("#btn-increase-break-minute").click(function () {
+        changeBreakTime(60);
+    });
+
+    $("#btn-reduce-break-second").click(function () {
+        changeBreakTime(-10);
+    });
+
+    $("#btn-increase-break-second").click(function () {
+        changeBreakTime(10);
+    });
+
+    function changeBreakTime(time) {
+        if ((breakTime + time >= 0) && (breakTime + time <= 6000)) {
+            stopAllClocks();
+            breakTime += time;
+            breakClock.setTime(breakTime);
+        }
+    }
+
 
     startButton.click(function () {
         if (isSessionStop) {
@@ -86,7 +133,25 @@ $(document).ready(function () {
 
     resetButton.click(function () {
         //stop the countdown when reset is clicked
-    });
-});
 
+        stopAllClocks();
+
+        sessionTime = resetSession;
+        breakTime = resetBreak;
+
+        sessionClock.setTime(sessionTime);
+        breakClock.setTime(breakTime);
+    });
+
+    function stopAllClocks(){
+        sessionClock.stop();
+        isSessionStop = true;
+
+        breakClock.stop();
+        isBreakStop = true;
+
+        startButton.text("Start");
+    }
+
+});
 
